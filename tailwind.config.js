b0af778d8d20362aeaf7255804a87ef1ed1bf15a
Alex Shyba <alex.shyba@uniform.dev>
2024-08-23 16:27:34 -0700

    feat: color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,35 @@
 /** @type {import('tailwindcss').Config} */
+
+import theme from "./tailwind.config.theme.json";
+import { generateTailwindcssColorKeysPattern } from "./utils/palette";
+
+const safelist = [
+  { pattern: /grid-cols-(1[0-2]|[1-9]|none|subgrid)/, variants: ["lg", "md"] },
+  {
+    pattern: /gap-(x|y)-(0(\.5)?|1(\.5)?|2(\.5)?|3(\.5)?|[1-9]?[0-9]|px)/,
+    variants: ["lg", "md"],
+  },
+  {
+    pattern: /(col|row)-start-(1[0-2]|[1-9]|none|subgrid)/,
+    variants: ["lg", "md"],
+  },
+  {
+    pattern: /(col|row)-(auto|span-(1[0-2]|[1-9]|full))/,
+    variants: ["lg", "md"],
+  },
+  { pattern: /justify-(start|center|end)/ },
+  {
+    pattern: /text-(xs|sm|base|lg|xl|2xl|3xl|4xl|5xl|6xl|7xl|8xl|9xl)/,
+    variants: ["lg", "md"],
+  },
+  { pattern: /text-(left|center|right)/, variants: ["lg", "md"] },
+];
+
+const colorKeys = Object.keys(theme.extend.colors || {});
+if (colorKeys.length) {
+  safelist.push(generateTailwindcssColorKeysPattern(colorKeys));
+}
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -6,8 +37,7 @@ module.exports = {
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  safelist,
+  theme,
+  plugins: [require("@tailwindcss/typography")],
 };
